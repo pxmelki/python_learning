5eebf9315f3390349cd99cbe3cebaaac6a9f9c5f
melki <>
2024-06-13 21:55:47 +0700

    day 2 learn python

---
+++ Day 2.py
@@ -0,0 +1,33 @@
+# variabel
+
+x = 22
+y = "mekos"
+print(x)
+print(y)
+
+x = 22          # x sekarang tipe int
+x = "mekos"     # x sekarang menjadi str
+print(x)
+print(x)
+
+x = str(22)     # x akan menjadi '22'
+y = int(22)     # y akan menjadi 22
+z = float(22)   # Z akan menjadi 22.0
+print(x)
+print(y)
+print(z)
+
+x = 22
+y = "mekos"
+print(type(x))
+print(type(y))
+
+x = "mekos"     # tanda kutip ganda sama dengan tanda kutip tunggal
+print(x)
+x = 'mekos'
+print(x)
+
+a = 22          # A tidak akan menimpa a
+A = "Mekos"
+print(a)
+print(A)
