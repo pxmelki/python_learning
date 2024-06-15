27f27c386bc470ef47087332607f5b3d84b40483
melki <mekiparkes@gmail.com>
2024-06-15 22:50:21 +0700

    day 4 learn python

---
+++ Day 4.py
@@ -0,0 +1,18 @@
+#Banyak Nilai ke Beberapa Variabel
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#Satu Nilai untuk Beberapa Variabel
+x = y = z = "blue"
+print(x)
+print(y)
+print(z)
+
+#Membongkar Koleksi
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
