fe0694b89ae080dc2d1cedc36d9393b4f6353cc7
melki <mekiparkes@gmail.com>
2024-06-16 22:38:41 +0700

    day 5 learn python

---
+++ Day 5.py
@@ -0,0 +1,28 @@
+# Output Variables
+
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x, y)
+
+x = 5
+y = "John"
+print(x + y)
+
+# Dalam fungsi print(), ketika Anda mencoba menggabungkan string dan angka dengan operator +, Python akan memberikan kesalahan:
